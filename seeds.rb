3f993a32f5ea441414d1338fe36ce55778dbaf59
Yannick Baudraz <yannick.baudraz@cpnv.ch>
2022-03-03 08:57:19 +0100

    Optimize seeders

--- seeds.rb
+++ seeds.rb
@@ -12,22 +12,18 @@ product4 = Product.create!(name: 'truck', price: 200, description: 'A truck', ca
 client = Client.create!(firstname: 'yannick', lastname: 'baudraz')
 Client.create!(firstname: 'john', lastname: 'doedoe')
 
-order1 = Order.new(status: 'PROGRESS')
-order1.order_items << [
-  OrderItem.new(quantity: 2, product: product1, order: order1),
-  OrderItem.new(quantity: 125, product: product3, order: order1),
-  OrderItem.new(quantity: 3, product: product4, order: order1)
-]
-order1.save!
-client.orders << order1
-client.save!
+order1 = client.orders.create(status: 'PROGRESS', order_items: [
+  OrderItem.new(quantity: 2, product: product1),
+  OrderItem.new(quantity: 125, product: product3),
+  OrderItem.new(quantity: 3, product: product4)
+])
 
 order2 = Order.new(status: 'PROGRESS')
 order2.order_items << [
-  OrderItem.new(quantity: 1, order: order2, product: product1),
-  OrderItem.new(quantity: 75, order: order2, product: product2),
-  OrderItem.new(quantity: 3, order: order2, product: product3),
-  OrderItem.new(quantity: 200, order: order2, product: product4)
+  OrderItem.new(quantity: 1, product: product1),
+  OrderItem.new(quantity: 75, product: product2),
+  OrderItem.new(quantity: 3, product: product3),
+  OrderItem.new(quantity: 200, product: product4)
 ]
 order2.save!
 client.orders << order2
